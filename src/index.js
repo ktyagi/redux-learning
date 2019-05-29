54e0339b50839861bbc1f45c7a63b0a8c3aef614
ktyagi <tyagitester@gmail.com>
2019-05-29 18:10:14 +1200

    colour display for box
    colour display for box

--- src/index.js
+++ src/index.js
@@ -3,43 +3,43 @@ import { render } from "react-dom";
 import Counter from "./Counter";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import App from "./App";
 
 import "./index.css";
-const initialState = {
-  count: 0
-};
-const App = () => (
-  <div>
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  </div>
-);
+// const initialState = {
+//   count: 0
+// };
+// const App = () => (
+//   <div>
+//     <Provider store={store}>
+//       <Counter />
+//     </Provider>
+//   </div>
+// );
 
+// function reducer(state = initialState, action) {
+//   console.log("reducer", state, action);
+//   // reducer undefined Object {type: "@@redux/INITh.p.s.r.9.k"}
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return {
+//         count: state.count + 1
+//       };
+//     case 'DECREMENT':
+//       return {
+//         count: state.count - 1
+//       };
+//     case 'RESET':
+//       return {
+//         count: 0
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
-function reducer(state = initialState, action) {
-  console.log("reducer", state, action);
-  // reducer undefined Object {type: "@@redux/INITh.p.s.r.9.k"}
-  switch(action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1
-      };
-    case 'DECREMENT':
-      return {
-        count: state.count - 1
-      };
-    case 'RESET':
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-console.log(store);
+// const store = createStore(reducer);
+// console.log(store);
 // Object {dispatch: function dispatch(), subscribe: function subscribe(), getState: function getState(), replaceReducer: function replaceReducer()}
 // store.dispatch({ type: "INCREMENT" });
 // store.dispatch({ type: "INCREMENT" });
